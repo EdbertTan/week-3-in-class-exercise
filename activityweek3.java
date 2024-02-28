268a562c36f426acea4197628dfe4f9c56201c34
EdbertTan <145570108+EdbertTan@users.noreply.github.com>
2024-02-28 12:52:39 +0700

    Add files via upload

---
+++ activityweek3.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class activityweek3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> elements = new ArrayList<Integer>();
+        elements.add(12);
+        elements.add(25);
+        elements.add(34);
+        elements.add(46);
+        elements.remove(1);
+        System.out.println(elements);
+    }
+}
